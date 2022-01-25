eb64f5af40e0cf51f8b870a47342094623210ebe
Kent Pedersen <webzone@kpdesign.dk>
2022-01-25 07:19:46 +0100

    Added parralelogram

--- src/app/parallelogram/parallelogram.component.ts
+++ src/app/parallelogram/parallelogram.component.ts
@@ -7,11 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParallelogramComponent implements OnInit {
 
-  constructor() { }
+  area: number = 0;
+  circum: number = 0;
+  lengthG: number = 0;
+  lengthS: number = 0;
+  height: number = 0;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  //There is no check if the numbers mach each other. so if both sides and height is filled, then they will calculate no matter what, and it can be wrong when
 
-  //Ran out of time. I'll try to get further in the morning. (21:57)
+  //Call the other two methods
+  calculate(lengthG: number, lengthS: number, height: number) {
+    this.calculateArea(lengthG, height);
+    this.calculateCircum(lengthG, lengthS);
+  }
+
+  //Calculate Area. But not filtering the circum. This is for version 2.0
+  calculateArea(lengthAB: number, height: number): number {
+    this.area = height * lengthAB;
+    return this.area;
+  }
+
+  //Calculating circum
+  calculateCircum(lengthG: number, lengthS: number): number {
+    this.circum = (2 * lengthG) + (2 * lengthS);
+    return this.circum;
+  }
+
+
 }
